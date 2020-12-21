a0b4fa7786b9f575ebc707790831cbd4b2aefe60
Icarus <astrikort24@gmail.com>
2020-12-21 20:56:24 +0200

    1.8 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,23 +1,37 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const Button = ({name,onClickHandler}) => {
-  return(
-    <button onClick = {onClickHandler}>{name}</button>
-  )
-}
+const Button = ({ name, onClickHandler }) => {
+  return <button onClick={onClickHandler}>{name}</button>;
+};
 
-const Display = ({name, clickedNo}) => {
+const Display = ({ name, clickedNo }) => {
   return (
-    <p>{name} {clickedNo}</p>
-  )
-}
+    <p>
+      {name} {clickedNo}
+    </p>
+  );
+};
 
+const Statistics = ({ good, neutral, bad, sum }) => {
+  const avg = (good - bad) / sum;
+  const positivePercentage = (100 * good) / sum;
+  return (
+    <>
+      <Display name="good" clickedNo={good} />
+      <Display name="neutral" clickedNo={neutral} />
+      <Display name="bad" clickedNo={bad} />
+      <Display name="all" clickedNo={sum} />
+      <Display name="average" clickedNo={avg} />
+      <Display name="positive" clickedNo={positivePercentage} />
+    </>
+  );
+};
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleGood = () => setGood(good + 1);
 
@@ -26,25 +40,21 @@ const App = () => {
   const handleBad = () => setBad(bad + 1);
 
   const sum = good + neutral + bad;
-  return (
 
+  return (
     <div>
       <h1>Give feedback</h1>
-      <Button name="good" onClickHandler={handleGood}/>
-      <Button name="neutral" onClickHandler={handleNeutral}/>
-      <Button name="bad" onClickHandler={handleBad}/>
+      <Button name="good" onClickHandler={handleGood} />
+      <Button name="neutral" onClickHandler={handleNeutral} />
+      <Button name="bad" onClickHandler={handleBad} />
       <h1>statistics</h1>
-      <Display name = "good" clickedNo = {good}/>
-      <Display name = "neutral" clickedNo = {neutral}/>
-      <Display name = "bad" clickedNo = {bad}/>
-      <Display name = "all" clickedNo = {sum}/>
-      <Display name = "average" clickedNo = {(good-bad)/sum}/>
-      <Display name = "positive" clickedNo = {100 * good / sum}/>
-
+      {sum > 0 ? (
+        <Statistics data={{ good, neutral, bad, sum }} />
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
