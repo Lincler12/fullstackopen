ed6b2e4e2ac33e235ade6f3847f0f25dc519152e
Icarus <astrikort24@gmail.com>
2020-12-21 21:47:46 +0200

    1.12 done

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Button = ({name, clickHandler}) => <button onClick = {clickHandler}>{name}</button>
+
+const Display = ({quote}) => <p>{quote}</p>
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const random = () => {
+   setSelected( Math.floor(Math.random() * anecdotes.length));
+   console.log(selected);
+  }
+  return (
+    <div>
+      <Display quote = {anecdotes[selected]}/>
+      <Button name = "random" clickHandler = {random}/>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
