61cb69583a034cffac9e2f58030f5bb3b482798b
Icarus <astrikort24@gmail.com>
2020-12-22 21:20:04 +0200

    2.10 done

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -12,6 +12,34 @@ const Display = ({ persons }) => {
   );
 };
 
+const Filter = ({ value, change }) => {
+  return (
+    <div>filter shown with:{<input value={value} onChange={change} />}</div>
+  );
+};
+
+const PersonForm = ({
+  formHandler,
+  nameValue,
+  changeNameValue,
+  numberValue,
+  changeNumberValue,
+}) => {
+  return (
+    <form onSubmit={formHandler}>
+      <div>
+        name: <input value={nameValue} onChange={changeNameValue} />
+      </div>
+      <div>
+        number: <input value={numberValue} onChange={changeNumberValue} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456" },
@@ -19,21 +47,23 @@ const App = () => {
     { name: "Dan Abramov", number: "12-43-234345" },
     { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-	const [searchValue, setSearchValue] = useState("");
-	const [showAll, setShowAll] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
   const onChangeNameHandler = (event) => setNewName(event.target.value);
 
   const onChangeNumberHandler = (event) => setNewNumber(event.target.value);
 
-	const onChangeSearchHandler = (event) => {
-		setSearchValue(event.target.value);
-		setShowAll(true);
-		if(event.target.value !== ""){
-			setShowAll(false);
-		}
-	}
+  const onChangeSearchHandler = (event) => {
+    setSearchValue(event.target.value);
+    setShowAll(true);
+    if (event.target.value !== "") {
+      setShowAll(false);
+    }
+  };
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -44,24 +74,22 @@ const App = () => {
     setPersons(persons.concat({ name: newName, number: newNumber }));
     setNewName("");
     setNewNumber("");
-	};
-	const displayArrayElements = showAll ? persons : persons.filter(person => person.name.indexOf(searchValue) > -1);
+  };
+  const displayArrayElements = showAll
+    ? persons
+    : persons.filter((person) => person.name.indexOf(searchValue) > -1);
   return (
     <div>
       <h2>Phonebook</h2>
-			<div>filter shown with: <input value = {searchValue} onChange={onChangeSearchHandler}/></div>
-			<h2>Add new</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          name: <input value={newName} onChange={onChangeNameHandler} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={onChangeNumberHandler} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter value={searchValue} change={onChangeSearchHandler} />
+      <h2>Add new</h2>
+      <PersonForm
+        formHandler={onSubmit}
+        nameValue={newName}
+        changeNameValue={onChangeNameHandler}
+        numberValue={newNumber}
+        changeNumberValue={onChangeNumberHandler}
+      />
       <h2>Numbers</h2>
       <Display persons={displayArrayElements} />
     </div>
